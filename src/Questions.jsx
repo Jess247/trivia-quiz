a63e59e4552a929e5548a7e0873a491c9845667d
Jess247 <jesss8692@hotmail.com>
2024-03-27 16:51:47 +0100

    Add sample data for styling purpose

--- src/Questions.jsx
+++ src/Questions.jsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
+import { questions } from "./test-data";
 
-function Questions {
-    return ()
+function Questions() {
+
+    const questionsElements = questions.map(question => {
+        const answerElements = question.answers.map(answer => <button className="answer-btns">{answer}</button>)
+        return (
+            <div className="question-container">
+                <h3 className="question">{question.question}</h3>
+                {answerElements}
+            </div>
+        )
+    }) 
+    return (
+        <main className="main-questions">
+            {questionsElements}
+            <button className="btn">Check answers</button>
+        </main>
+    )
 }
 
 export default Questions
---
+++ src/test-data.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+        question: "How would one say goodbye in spanish?",
+        answers: ["Adios", "Hola","Au Revoir", "Salir"]
+    },
+    {
+        question: "How would one say goodbye in spanish?",
+        answers: ["Adios", "Hola","Au Revoir", "Salir"]
+    },
+    {
+        question: "How would one say goodbye in spanish?",
+        answers: ["Adios", "Hola","Au Revoir", "Salir"]
+    },
+    {
+        question: "How would one say goodbye in spanish?",
+        answers: ["Adios", "Hola","Au Revoir", "Salir"]
+    },
+    {
+        question: "How would one say goodbye in spanish?",
+        answers: ["Adios", "Hola","Au Revoir", "Salir"]
+    }
+]
